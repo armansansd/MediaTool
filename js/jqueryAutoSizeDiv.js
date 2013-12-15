a56a5f874f20af2c43bf3a22987ae486fd9ea9d1
armansansd <a.mohtadji@gmail.com>
2013-12-15 17:27:11 +0100

    add files

---
+++ js/jqueryAutoSizeDiv.js
@@ -0,0 +1,68 @@
+	 	function printFrame(){
+			window.print();
+		}
+
+	//==============================================presLivre
+		$(function(){
+				$('.titre').autosize();
+				$('.auteur').autosize();
+				$('.date').autosize();
+				$('.informations_droite1').autosize();
+				$('.informations_droite2').autosize();
+				$('.informations_droite3').autosize();
+				$('.informations_gauche1').autosize();
+				$('.informations_gauche2').autosize();
+				$('.informations_gauche3').autosize();
+				$('.informations_gauche4').autosize();
+				$('.informations_droite4').autosize();
+				$('.resume').autosize();
+			});
+
+	//==================================================presLivre A3
+
+				$(function(){
+				$('.titre2').autosize();
+				$('.auteur2').autosize();
+				$('.date2').autosize();
+				$('.informations2_droite1').autosize();
+				$('.informations2_droite2').autosize();
+				$('.informations2_droite3').autosize();
+				$('.informations2_gauche1').autosize();
+				$('.informations2_gauche2').autosize();
+				$('.informations2_gauche3').autosize();
+				$('.informations2_gauche4').autosize();
+				$('.resume2').autosize();
+			});
+
+	
+	//=========================================================event A4
+
+		$(function(){
+				$('.type3').autosize();
+				$('.intervenant3').autosize();
+				$('.titre3').autosize();
+				$('.date3').autosize();
+				$('.horaire3').autosize();
+				$('.lieu3').autosize();	
+			});
+				
+	//=========================================================event A3
+
+		$(function(){
+				$('.type4').autosize();
+				$('.intervenant4').autosize();
+				$('.titre4').autosize();
+				$('.date4').autosize();
+				$('.horaire4').autosize();
+				$('.lieu4').autosize();
+				$('.texte4').autosize();	
+			});
+
+	//=========================================================info A3
+		
+		$(function(){
+				$('.titre6').autosize();
+				$('.texte6').autosize();
+			});
+
+
